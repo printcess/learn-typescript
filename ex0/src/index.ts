242d53603195b55d800dceafab7c28b2cd2f4adf
printcess <pngerdchai@gmail.com>
2025-04-02 21:47:53 +0700

    function

--- ex0/src/index.ts
+++ ex0/src/index.ts
@@ -1,1 +1,14 @@
-console.log("hello world")
+var score: number = 21;
+function gradecal(x: number): string {
+    if (x >= 80) {
+        return ("grade 4");
+    }   else if (x < 80 && x >= 75) {
+        return ("grede 3.5"); 
+    }   else if (x < 75 && x >= 70) {
+        return ("grede 3"); 
+    }   else {
+        return ("grede 1"); 
+    }
+}
+var result: string = gradecal(score)
+console.log(result)
